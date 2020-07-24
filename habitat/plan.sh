770500478636b75fc321340d1342ddd020cc942d
FaizanSRE <ffulara@msystechnologies.com>
2020-07-24 20:41:56 +0530

    add habitat package

---
+++ habitat/plan.sh
@@ -0,0 +1,8 @@
+pkg_name=meme-machine
+pkg_origin=ff_chef
+pkg_version="0.1.0"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+
+pkg_scaffolding="core/scaffolding-ruby"
+pkg_deps=( core/imagemagick )
+
